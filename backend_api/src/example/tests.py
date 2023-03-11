163971b08df2442b6379cb0acf14207ea48e46bc
dd84ai <dd84ai@gmail.com>
2023-03-11 18:58:22 +0100

    feat: hooking up simple testing

--- backend_api/src/example/tests.py
+++ backend_api/src/example/tests.py
@@ -1,6 +1,9 @@
 import pytest
+from fastapi.testclient import TestClient
 
 
 @pytest.mark.asyncio
-async def test_example() -> None:
-    assert True
+async def test_ping_example(client: TestClient) -> None:
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "pong!"}
